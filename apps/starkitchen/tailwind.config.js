d5eee4d9f0d8be5efe5bceadbf18619d0fa2d7b1
madmonl-starkware <liad@starkware.co>
2024-12-10 17:12:20 +0200

    Fix commonjs issue

--- apps/starkitchen/tailwind.config.js
+++ apps/starkitchen/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
@@ -85,5 +85,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [import('tailwindcss-animate')],
 };
